8a0152dec5e7c2e1659ead7c8e817e0919bbf184
Krish Ravindranath <krish.ravindranath@gmail.com>
2019-02-09 17:07:17 -0500

    debug

--- test_obd_pi_data.py
+++ test_obd_pi_data.py
@@ -31,11 +31,26 @@ counter = 0
 while (time.time() - initial < 20):
     timeref = time.time()
     timestep = .5
-    if ((time.time() - timeref) < timestep): 
-        led1.value = float(speed[counter]) / 50.0
-        led2.value = (float(rpm[counter]) - 500.0) / 1500.0
-        led3.value = (float(throttle[counter]) - 5.0) / 30.0
-        led4.value = float(speed[counter]) / 50.0
+    if ((time.time() - timeref) < timestep):
+        
+        val =  float(speed[counter]) / 50.0
+        print val
+        led1.value = val
+
+        
+        val = (float(rpm[counter]) - 500) / 1500
+        print val
+        led2.value = val
+
+        
+        val = (float(throttle[counter]) - 5.0) / 30.0
+        print val
+        led3.value = val
+        
+        val = float(speed[counter]) / 50.0
+        print val
+        led4.value = val
+
         counter += 1
     if counter >= len(speed):
         counter = 0
