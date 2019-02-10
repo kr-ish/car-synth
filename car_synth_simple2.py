5e9942a4b8ba0e7ebeb9ad19bc1f5ef6b7171972
Krish Ravindranath <krish.ravindranath@gmail.com>
2019-02-09 23:09:19 -0500

    add test vals, clipped vals

--- car_synth_simple2.py
+++ car_synth_simple2.py
@@ -22,45 +22,58 @@ vca_next_val = 0.0
 vcf_next_val = 0.0
 vco_next_val = 0.0
 lfo_next_val = 0.0
+vca_clipped_val = 0.0
+vcf_clipped_val = 0.0
+vco_clipped_val = 0.0
+lfo_clipped_val = 0.0
+
 
 # main
 while (True):
 
     # Speed + RPM to VCO
-    speed_resp = connection.query(speed_cmd, force=True)
-    fuel_trim_resp = connection.query(fuel_trim_cmd, force=True)
-    rpm_resp = connection.query(rpm_cmd, force=True)
+    # speed_resp = connection.query(speed_cmd, force=True)
+    # fuel_trim_resp = connection.query(fuel_trim_cmd, force=True)
+    # rpm_resp = connection.query(rpm_cmd, force=True)
 
-    if speed_resp.is_null():
-        print('speed_resp null')
-        continue
-    elif rpm_resp.is_null():
-        print('rpm_resp null')
-        continue
-    elif fuel_trim_resp.is_null():
-        print('fuel_trim_resp null')
-        continue
+    # if speed_resp.is_null():
+    #     print('speed_resp null')
+    #     continue
+    # elif rpm_resp.is_null():
+    #     print('rpm_resp null')
+    #     continue
+    # elif fuel_trim_resp.is_null():
+    #     print('fuel_trim_resp null')
+    #     continue
 
-    speed_val = float(speed_resp.value.magnitude)
-    rpm_val = float(rpm_resp.value.magnitude)
-    fuel_trim_val = float(fuel_trim_resp.value.magnitude)
+    # speed_val = float(speed_resp.value.magnitude)
+    # rpm_val = float(rpm_resp.value.magnitude)
+    # fuel_trim_val = float(fuel_trim_resp.value.magnitude)
+    speed_val = 30.0
+    rpm_val = 1500
+    fuel_trim_val = 5.0
+
 
     # vco_next_value = (speed_val / 100 + .3) + \
     #     ((random.rand() - .5) * (3.0/5)) * (rpm_val  - 480) / 1720
-    vco_next_value = (speed_val / 100 + .3) \
+    vco_next_value = (speed_val / 160 + .3) \
         + ((fuel_trim_val + 9) / 20 - .5) * (3.0/5) * (rpm_val  - 480) / 1720
     if vco_next_value > 1:
-        vco_next_value = 1.0
+        vco_clipped_value = 1.0
     elif vco_next_value < 0:
-        vco_next_value = 0.0
-    print('vco_next_value, {}'.format(vco_next_value))
-    vco.value = vco_next_value
+        vco_clipped_value = 0.0
+    else:
+        vco_clipped_value = vco_next_value
+    print('vco_next_value {}, vco_clipped_value {}'.format(vco_next_value, vco_clipped_value))
+    vco.value = vco_clipped_value
 
     # Fuel Trim to LFO
     lfo_next_value = (rpm_val - 480) / 1720
     if lfo_next_value > 1:
-        lfo_next_value = 1.0
+        lfo_clipped_value = 1.0
     elif lfo_next_value < 0:
-        lfo_next_value = 0.0
-    print('lfo_next_value, {}'.format(lfo_next_value))
-    lfo.value = lfo_next_value
+        lfo_clipped_value = 0.0
+    else:
+        lfo_clipped_value = lfo_next_value
+    print('lfo_next_value {}, lfo_clipped_value {}'.format(lfo_next_value, lfo_clipped_value))
+    lfo.value = lfo_clipped_value
