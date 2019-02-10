f9d28636c6fc53c63ee76c96e5dafd297f75f109
Krish Ravindranath <krish.ravindranath@gmail.com>
2019-02-09 21:43:27 -0500

    fix btooth

--- bluetooth.sh
+++ bluetooth.sh
@@ -5,20 +5,20 @@ set address [lindex $argv 0]
 
 spawn sudo bluetoothctl -a
 expect -re $prompt
-send "remove $address\r"
-sleep 1
-expect -re $prompt
-send "scan on\r"
-send_user "\nSleeping\r"
-sleep 5
-send_user "\nDone sleeping\r"
-send "scan off\r"
-expect "Controller"
+# send "remove $address\r"
+# sleep 1
+# expect -re $prompt
+# send "scan on\r"
+# send_user "\nSleeping\r"
+# sleep 5
+# send_user "\nDone sleeping\r"
+# send "scan off\r"
+# expect "Controller"
 send "trust $address\r"
 sleep 2
 send "pair $address\r"
 sleep 2
-send "0000\r"
+send "1234\r"
 sleep 3
 send_user "\nShould be paired now.\r"
 send "quit\r"
