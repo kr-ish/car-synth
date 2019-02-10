2f6ec725dd45f811c6b16aa177a37a290e39cabf
Krish Ravindranath <krish.ravindranath@gmail.com>
2019-02-10 15:47:16 +0000

    working switches

--- switch_test.py
+++ switch_test.py
@@ -8,12 +8,14 @@ mode_switch = Button(27, pull_up=True)  # connect to GND
 
 # switch on
 on_switch.wait_for_press()
+print('on switch pressed')
 
 # main
 while (True):
 
-    while mode_switch.is_pressed():
+    while(mode_switch.is_pressed):
         print('alg mode')
         sleep(0.1)
-    while not mode_switch.is_pressed():
+    while(not mode_switch.is_pressed):
+        print('simple mode')
         sleep(0.1)
