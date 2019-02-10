c5112846be2874d90dfb011998982c158d1e6db9
Krish Ravindranath <krish.ravindranath@gmail.com>
2019-02-10 03:37:30 -0500

    proto switch test script

---
+++ switch_test.py
@@ -0,0 +1,19 @@
+from gpiozero import Button
+from time import sleep
+
+# Setup GPIO
+on_switch = Button(24, pull_up=True)  # connect to GND
+mode_switch = Button(27, pull_up=True)  # connect to GND
+
+
+# switch on
+on_switch.wait_for_press()
+
+# main
+while (True):
+
+    while mode_switch.is_pressed():
+        print('alg mode')
+        sleep(0.1)
+    while not mode_switch.is_pressed():
+        sleep(0.1)
