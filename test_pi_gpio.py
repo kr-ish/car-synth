3ed55757858e23ce4639d633fdfe4cce30e21833
Krish Ravindranath <krish.ravindranath@gmail.com>
2019-02-09 22:18:33 -0500

    full test

--- test_pi_gpio.py
+++ test_pi_gpio.py
@@ -2,8 +2,10 @@ from gpiozero import PWMLED, Button
 from signal import pause
 from time import sleep
 
-led = PWMLED(17, frequency=490)
-# button = Button(3)
+out_1 = PWMLED(17, frequency=490)
+out_2 = PWMLED(18, frequency=490)
+out_3 = PWMLED(22, frequency=490)
+out_4 = PWMLED(23, frequency=490)
 
 # button.when_pressed = led.on
 # button.when_released = led.off
@@ -14,6 +16,9 @@ value = 0.0
 while True:
     value += 0.05
     value = value % 1.0
-    led.value = value
+    out_1.value = value
+    out_2.value = value
+    out_3.value = value
+    out_4.value = value
     sleep(0.1)
 
